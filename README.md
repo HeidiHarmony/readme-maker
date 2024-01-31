06d0cb9f1ef16c9414c61bee4ebef7289477f6a6
HeidiHarmony <tenibrae@gmail.com>
2024-01-30 21:10:26 -0500

    First three questions added, readme file structured, initial test

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
-# Professional README Generator Starter Code
+# README-Maker
+## Saving you time for the good stuff... coding, of course!
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+### Find it here: 
+
+- [About](#about)
+- [Features](#features)
+- [Documentation](#documentation)
+- [Future Development](#future)
+- [Report Issues](#report)
+- [How to Contribute](#contribute)
+- [License](#license)
+- [Author](#author)
+
+
+## About
+- What is it?
+- Why use README-Maker (What problem does it solve)
+- How README-Maker Works
+- What I learned along the way
+    - Challenges
+
+
+## Features
+- What makes README-Maker stand out
+### Watch README-Maker in Action
+
+## Documentation
+ ### Installation
+ - Required "dependencies"?
+ ### Getting Started
+
+## Future Development
+
+## Report Issues
+
+## How to Contribute
+
+## License
+
+## Author
---
+++ index.js
@@ -0,0 +1,54 @@
+// TODO: Packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+var questions = [];
+
+inquirer
+    .prompt ([
+    //Name of App (app-name)
+    { 
+        type: 'input',
+        name: 'app-name',
+        message: 'Welcome to README-Maker! We save you time in creating your readme documentation so you can spend more time on the important stuff! Let\'s get started. What is the name of your application?',
+   }
+   //Tagline (tagline)
+   {
+        type: 'input',
+        name: 'tagline',
+        message: 'What is your application\'s tagline or flavor text? (type SKIP to skip):',
+        validate: function (input) {
+            if (input.toLowerCase() === 'skip') {
+              return true; // Skip the question
+            }
+            return input !== '' || 'Please enter a value or "skip"';
+          },
+    },
+    //Link to deployed application
+    {
+        type: 'input',
+        name: 'link',
+        message: 'What is your application\'s URL?',
+    }
+    //About
+    {
+        type: 'input',
+        name: 'link',
+        message: 'What is your application\'s URL?',
+    }
+])
+    .then(answers => {
+        // something here?
+        console.log(answers);
+    })
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
