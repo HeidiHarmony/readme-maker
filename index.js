6057912d2d938383b019bef46b0030508a52a4eb
Heidi Harmony Carrier <tenibrae@gmail.com>
2024-02-07 18:07:40 -0500

    Fixing variables not used and declared functions not called

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-var questions = [
+
 var questions = [
 //Name of App (app-name)
 { 
